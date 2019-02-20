871d556b190e427ea342c2d1cf8752e18f4da9d5
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-02-20 20:05:10 +1300

    Delete slack history example.

---
+++ examples/slack/clean.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+
+require 'pry'
+require 'set'
+
+def rate_limited?(response)
+	pp response
+	
+	response[:error] == "ratelimited"
+end
+
+require 'async/rest/resource'
+
+URL = "https://slack.com/api"
+TOKEN = "xoxp-your-api-token"
+
+Async::REST::Resource.for(URL) do |resource|
+	authenticated = resource.with(parameters: {token: TOKEN})
+	delete = authenticated.with(path: "chat.delete")
+	
+	page = 1
+	while true
+		search = authenticated.with(path: "search.messages", parameters: {page: page, count: 100, query: "from:@username before:2019-02-15"})
+		representation = search.get
+		
+		messages = representation.value[:messages]
+		matches = messages[:matches]
+		
+		puts "Found #{matches.count} messages on page #{page}..."
+		
+		break if matches.empty?
+		
+		matches.each do |message|
+			text = message[:text]
+			channel_id = message[:channel][:id]
+			channel_name = message[:channel][:name]
+			timestamp = message[:ts]
+			
+			pp [timestamp, channel_name, text]
+			
+			message_delete = Async::REST::Representation.new(
+				delete.with(parameters: {channel: channel_id, ts: timestamp})
+			)
+			
+			response = message_delete.post
+			if rate_limited?(response.read)
+				puts "Rate limiting..."
+				Async::Task.current.sleep 1
+			end
+		end
+		
+		page += 1
+	end
+end
+
+puts "Done"
