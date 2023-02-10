d8d71b4b9ad9ab350709e5a4f5cb425b89852ad4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-02-11 11:52:55 +1300

    Improved test.

--- test/async/rest/resource.rb
+++ test/async/rest/resource.rb
@@ -6,18 +6,16 @@
 require 'async/rest/resource'
 
 describe Async::REST::Resource do
-	let(:url) {'http://example.com/'}
+	let(:url) {'http://example.com'}
 	let(:resource) {subject.for(url)}
 	
 	it 'can update path' do
 		expect(resource.reference.path).to be == '/'
 		
-		resource.with(path: 'foo') do |foo_resource|
-			expect(foo_resource.reference.path).to be == '/foo'
-			
-			foo_resource.with(path: 'bar') do |bar_resource|
-				expect(bar_resource.reference.path).to be == '/foo/bar'
-			end
-		end
+		foo_resource = resource.with(path: '/foo')
+		expect(foo_resource.reference.path).to be == '/foo'
+		
+		bar_resource = foo_resource.with(path: 'bar')
+		expect(bar_resource.reference.path).to be == '/foo/bar'
 	end
 end
